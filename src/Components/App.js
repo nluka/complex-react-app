a82f909858cbbe36ecaf20e8fc4083b979eaa5d4
Nicholas Lukasevich <n.luka784@gmail.com>
2021-05-15 09:07:47 -0400

    Lesson 17

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,14 +1,28 @@
 import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
 import Header from './Header/Header';
 import HomeGuest from './HomeGuest/HomeGuest';
 import Footer from './Footer/Footer';
+import About from './About/About';
+import Terms from './Terms/Terms';
 
 export default function App() {
   return (
-    <>
+    <BrowserRouter>
       <Header />
-      <HomeGuest />
+      <Switch>
+        <Route path='/' exact>
+          <HomeGuest />
+        </Route>
+        <Route path='/about'>
+          <About />
+        </Route>
+        <Route path='/terms'>
+          <Terms />
+        </Route>
+      </Switch>
       <Footer />
-    </>
+    </BrowserRouter>
   );
 }
