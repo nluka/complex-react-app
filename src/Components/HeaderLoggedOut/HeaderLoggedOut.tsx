5ea2683172f132bed735407c043b1b9c7234b770
Nicholas Lukasevich <n.luka784@gmail.com>
2021-05-30 03:42:39 -0400

    Lesson 29

---
+++ src/Components/HeaderLoggedOut/HeaderLoggedOut.tsx
@@ -0,0 +1,55 @@
+import axios from 'axios';
+import React, { useState } from 'react';
+
+const HeaderLoggedOut = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    try {
+      const res = await axios.post('http://localhost:8080/login', {
+        username,
+        password,
+      });
+      if (res.data === false) {
+        console.log('Invalid credentials.');
+        return;
+      }
+      console.log(res.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit} className='mb-0 pt-2 pt-md-0'>
+      <div className='row align-items-center'>
+        <div className='col-md mr-0 pr-md-0 mb-3 mb-md-0'>
+          <input
+            name='username'
+            className='form-control form-control-sm input-dark'
+            type='text'
+            placeholder='Username'
+            autoComplete='off'
+            onChange={(e) => setUsername(e.target.value)}
+          />
+        </div>
+        <div className='col-md mr-0 pr-md-0 mb-3 mb-md-0'>
+          <input
+            name='password'
+            className='form-control form-control-sm input-dark'
+            type='password'
+            placeholder='Password'
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <div className='col-md-auto'>
+          <button className='btn btn-success btn-sm'>Sign In</button>
+        </div>
+      </div>
+    </form>
+  );
+};
+
+export default HeaderLoggedOut;
