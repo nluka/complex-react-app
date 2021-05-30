b84401ca19b2e5b05195638d3d4289aba28ae104
Nicholas Lukasevich <n.luka784@gmail.com>
2021-05-30 04:24:03 -0400

    Lesson 32

--- src/Components/App.tsx
+++ src/Components/App.tsx
@@ -1,19 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import Header from './Header/Header';
-import HomeGuest from './HomeGuest/HomeGuest';
 import Footer from './Footer/Footer';
 import About from './About/About';
 import Terms from './Terms/Terms';
+import Home from './Home/Home';
+import HomeGuest from './HomeGuest/HomeGuest';
 
 export default function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(
+    localStorage.getItem('token') !== null ? true : false
+  );
+
   return (
     <BrowserRouter>
-      <Header />
+      <Header isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn} />
       <Switch>
         <Route path='/' exact>
-          <HomeGuest />
+          {isLoggedIn ? <Home /> : <HomeGuest />}
         </Route>
         <Route path='/about'>
           <About />
--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -1,13 +1,14 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 import HeaderLoggedIn from '../HeaderLoggedIn/HeaderLoggedIn';
 import HeaderLoggedOut from '../HeaderLoggedOut/HeaderLoggedOut';
 
-export default function Header() {
-  const [isLoggedIn, setIsLoggedIn] = useState(
-    localStorage.getItem('token') !== null ? true : false
-  );
+interface Props {
+  isLoggedIn: boolean;
+  setIsLoggedIn: Function;
+}
 
+export default function Header(props: Props) {
   return (
     <header className='header-bar bg-primary mb-3'>
       <div className='container d-flex flex-column flex-md-row align-items-center p-3'>
@@ -17,10 +18,10 @@ export default function Header() {
             ComplexApp{' '}
           </Link>
         </h4>
-        {isLoggedIn ? (
-          <HeaderLoggedIn setIsLoggedIn={setIsLoggedIn} />
+        {props.isLoggedIn ? (
+          <HeaderLoggedIn setIsLoggedIn={props.setIsLoggedIn} />
         ) : (
-          <HeaderLoggedOut setIsLoggedIn={setIsLoggedIn} />
+          <HeaderLoggedOut setIsLoggedIn={props.setIsLoggedIn} />
         )}
       </div>
     </header>
